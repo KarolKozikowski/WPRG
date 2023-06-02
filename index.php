4abe1408ab1f390b187f7898794d94f230782657
KarolKozikowski <134882954+KarolKozikowski@users.noreply.github.com>
2023-06-03 01:06:17 +0200

    Getting started with home page I guess

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="CSS/style.css">
+        <title>Arctic Airlines</title>
+    </head>
+    <body>
+        <header>
+            <img class="logo" src="art/logo2.png">
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="#">Connections</a></li>
+                    <li><a href="#">Book Flight</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+            <a href="#"><button class="login_button">Sign in</button></a>
+        </header>
+        <section class="main_sectoin">
+            <h1>Arctic Airlines</h1>
+            <p>The gateway to The North</p>
+            <a href="#" class="main_button">Get Started</a>
+        </section>
+        <footer>
+            <p>&copy; 2023 Karol Kozikowski. Not all rights reserved.</p>
+        </footer>
+    </body>
+</html>
